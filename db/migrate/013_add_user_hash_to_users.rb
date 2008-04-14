286fa524645b5ca23809b671597e39b715141874
Justin Perkins <justin@habanero.gateway.2wire.net>
2008-04-13 19:32:42 -0500

    do not generate the has inside the migration, since dreamhost keeps killing my process

--- db/migrate/013_add_user_hash_to_users.rb
+++ db/migrate/013_add_user_hash_to_users.rb
@@ -1,12 +1,6 @@
 class AddUserHashToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :secret_hash, :string, :limit => 255
-    User.find(:all).each do |user|
-      puts "Saving #{ user.id } with hash"
-      user.build_secret_hash
-      puts "Hash is: #{ user.secret_hash }"
-      user.save
-    end
   end
 
   def self.down
